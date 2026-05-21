76d0f517b04c096a8a647220e9c6fc01f0e69952
Marek 'seqre' Grzelak <git@seqre.dev>
2026-05-21 16:03:10 +0200

    chore: fix miri

--- src/secure_utils.rs
+++ src/secure_utils.rs
@@ -2,12 +2,14 @@
 pub mod memlock {
     extern crate libc;
 
+    #[allow(unused_variables)]
     pub fn mlock<T>(data: *mut T, count: usize) {
         let byte_num = count * std::mem::size_of::<T>();
         // SAFETY: `cont` points to a valid allocation of at least `count *
         // size_of::<T>()` bytes (guaranteed by callers passing pointers from
         // live Vec/Array/Box allocations). mlock/madvise are safe to call on
         // any valid memory region.
+        #[cfg(not(test))] // unsupported operation: can't call foreign function `mlock` on OS `linux
         unsafe {
             let ptr = data.cast::<libc::c_void>();
             libc::mlock(ptr, byte_num);
@@ -18,11 +20,13 @@ pub mod memlock {
         }
     }
 
+    #[allow(unused_variables)]
     pub fn munlock<T>(data: *mut T, count: usize) {
         let byte_num = count * std::mem::size_of::<T>();
         // SAFETY: Same as `mlock` - the pointer is to a valid allocation that was
         // previously locked. munlock/madvise are safe to call on any valid
         // memory region.
+        #[cfg(not(test))] // unsupported operation: can't call foreign function `mlock` on OS `linux
         unsafe {
             let ptr = data.cast::<libc::c_void>();
             libc::munlock(ptr, byte_num);
